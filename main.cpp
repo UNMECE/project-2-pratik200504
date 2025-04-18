cc26e20f1a592c0dedade802665d31ec44030022
pratik200504 <pojha@unm.edu>
2025-04-18 12:24:15 -0600

    saved

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
+#include<vector>
+#include<fstream>
+#include<string>
 
 class Student{
+protected:
+    std::string first_name;
+    std::string last_name;
+    double gpa;
+    int grad_year;
+    std::string grad_semester;
+    int enrolled_year;
+    std::string enrolled_semester;
+    std::string level; // undergrad or grad
+
+public:
+
+
 
